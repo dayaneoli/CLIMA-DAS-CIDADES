dfbcf1bfb2cd84cc8b3e8b9d1bf49519b975465d
dayaneoli <100381919+dayaneoli@users.noreply.github.com>
2022-06-26 23:08:13 +0300

    Subindo a primeira versão
    API que mostra os dados climáticos da cidade de Natal/RN.

---
+++ requestTest.js
@@ -0,0 +1,14 @@
+
+var request = require('request'); //require do módulo "request" do node.js.
+
+request('http://api.openweathermap.org/data/2.5/weather?q=Natal&APPID=cf330a399350226453d27f0a660cf2ad', function (error, response, body) {
+  console.log ('erro:', error);  // Imprime o erro ocorrido (de conexão)
+ 
+  console.log ('statusCode:', response && response.statusCode);  // Imprime o código de status da resposta se uma resposta foi recebida
+  
+  // console.log ('body:', body);  // Imprime o HTML da página acima.
+  var parsedWeather = JSON.parse(body);
+
+  console.log("Os dados climáticos atuais de Natal/RN são:", parsedWeather); 
+
+});
